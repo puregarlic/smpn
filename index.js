01ceea3c15af8cfa75f6aa607235da46cd8e900d
Graham Barber <grahammbarber@gmail.com>
2017-07-27 22:16:50 -0700

    Well, it searches now

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 #!/usr/local/bin/node
 
-console.log('You searched for somethin')
+const got = require('got')
+const program = require('commander')
+const inquirer = require('inquirer')
+
+program.version('1.0.0').usage('[options] <search terms>').parse(process.argv)
+
+const plusify = (strings, args) => {
+  return args[0].replace(/ /, '+')
+}
+
+const search = terms => {
+  got('https://api.npms.io/v2/search?q=' + plusify`${terms}`)
+    .then(res => {
+
+      const results = JSON.parse(res.body)
+      const choices = results.results.map(n => n.package.name)
+
+      inquirer.prompt({
+        type: 'list',
+        name: 'Results',
+        message: 'Here is what we found:',
+        choices
+      })
+    })
+    .catch(e => console.log(e))
+}
+
+search(program.args)
