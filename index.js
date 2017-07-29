840bc14daca558be3a254c27b5faf969bc95275f
Graham Barber <grahammbarber@gmail.com>
2017-07-28 23:24:37 -0700

    Fixed weird package urls, bumped to version 1.0.4

--- index.js
+++ index.js
@@ -7,33 +7,35 @@ const inquirer = require('inquirer')
 
 program.version('1.0.0').usage('[options] <search terms>').parse(process.argv)
 
-const plusify = (strings, args) => {
-  if (!args[0]) {
+const validate = (strings, terms) => {
+  if (!terms[0]) {
     console.error('You need to pass search terms!')
     process.exit()
-}
-  return args[0].replace(/ /, '+')
+  }
+  return strings[0] + (typeof terms == 'array' ? terms.join('+') : terms)
 }
 
 const search = terms => {
-  got('https://api.npms.io/v2/search?q=' + plusify`${terms}`)
+  got(validate`https://api.npms.io/v2/search?q=${terms}`)
     .then(res => {
-
       const results = JSON.parse(res.body)
-      const choices = results.results.map(n => n.package.name)
+      let choices = results.results.map(n => n.package.name)
 
-      inquirer.prompt({
-        type: 'list',
-        name: 'lib',
-        message: 'Here is what we found:',
-        choices
-      }).then(answer => {
-        got('https://api.npms.io/v2/package/' + plusify`${answer.lib}`)
-          .then(desc => {
+      inquirer
+        .prompt({
+          type: 'list',
+          name: 'lib',
+          message: 'Here is what we found:',
+          choices: [ new inquirer.Separator(), ...choices ]
+        })
+        .then(answer => {
+          got(
+            validate`https://api.npms.io/v2/package/${answer.lib}`
+          ).then(desc => {
             const results = JSON.parse(desc.body)
             open(results.collected.metadata.links.npm)
           })
-      })
+        })
     })
     .catch(e => console.log(e))
 }
