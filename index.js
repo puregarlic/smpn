8b264a501a1c4b700c6ed395b6b5c31254721530
Graham Barber <grahammbarber@gmail.com>
2017-07-29 01:42:01 -0700

    Added display for version, scores, and description

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const got = require('got')
 const open = require('open')
+const chalk = require('chalk')
 const program = require('commander')
 const inquirer = require('inquirer')
 const pkg = require('./package.json')
@@ -19,14 +20,45 @@ const validate = (strings, terms) => {
   return strings[0] + (typeof terms == 'array' ? terms.join('+') : terms)
 }
 
+const longest = a => {
+  let c = 0,
+    d = 0,
+    l = 0,
+    i = a.length
+  if (i)
+    while (i--) {
+      d = a[i].length
+      if (d > c) {
+        l = i
+        c = d
+      }
+    }
+  return a[l].length
+}
+
 const search = terms => {
   got(validate`https://api.npms.io/v2/search?q=${terms}`)
     .then(res => {
       const results = JSON.parse(res.body)
       if (results.total === 0) return console.error('No results found!')
 
+      const longestString = longest(results.results.map(n => n.package.name))
+
       const choices = results.results.map(n => ({
-        name: n.package.name,
+        name: `${chalk.bold(
+          n.package.name.length < longestString
+            ? n.package.name.padEnd(longestString) +
+              chalk.gray.dim('(' + n.package.version + ')')
+            : n.package.name + chalk.gray.dim('(' + n.package.version + ')')
+        )} ${chalk.green.dim(
+          Math.floor(n.score.detail.quality * 100) + '%'
+        )} ${chalk.yellow.dim(
+          Math.floor(n.score.detail.popularity * 100) + '%'
+        )} ${chalk.blue.dim(
+          Math.floor(n.score.detail.maintenance * 100) + '%'
+        )} ${chalk.bgRed(Math.floor(n.score.final * 100) + '%')} ${chalk.dim(
+          n.package.description
+        )}`,
         value: {
           url: n.package.links.npm
         }
@@ -36,7 +68,10 @@ const search = terms => {
         .prompt({
           type: 'list',
           name: 'lib',
-          message: 'Here is what we found:',
+          message: `Here is what we found:
+${chalk.green.dim('<Quality>')} ${chalk.yellow.dim(
+            '<Popularity>'
+          )} ${chalk.blue.dim('<Maintenance>')} ${chalk.bgRed('<Overall>')}`,
           choices: [ ...choices, new inquirer.Separator() ]
         })
         .then(answer => {
