5fc2e84622bb299d8f8b482de1875114c7064236
Graham Barber <grahammbarber@gmail.com>
2017-08-11 23:19:36 -0700

    Refactored rendering code

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ program
   .usage('[options] [search terms]')
   .option('-a --author [name]', 'Search for a specific author')
   .option('-s --scope [scope]', 'Restrict search to a specific scope')
-  .option('-k --keywords [keywords]', 'Filter results to packages with the specific keywords. Comma seperated, no spaces in between!')
+  .option(
+    '-k --keywords [keywords]',
+    'Filter results to packages with the specific keywords. Comma seperated, no spaces in between!'
+  )
   .option('-d --not-deprecated', 'Restrict to non-deprecated results')
   .option('-D --deprecated', 'Restrict to deprecated results')
   .option('-u --not-unstable', 'Restrict to results above v1.0.0')
@@ -20,20 +23,35 @@ program
   .option('-i --not-insecure', 'Restrict to secure results')
   .option('-I --insecure', 'Restrict to insecure/vulnerable results')
   .option('-b --no-boost-exact', 'Do not boost exact search matches')
-  .option('-e --score-effect [number]', 'Set the score effect value. Default: 15.3')
-  .option('-q --quality-weight [number]', 'Specify a quality weight. Default: 1.95')
-  .option('-p --popularity-weight [number]', 'Specify a popularity weight. Default: 3.3')
-  .option('-m --maintenance-weight [number]', 'Specify a maintenance weight. Default: 2.05')
+  .option(
+    '-e --score-effect [number]',
+    'Set the score effect value. Default: 15.3'
+  )
+  .option(
+    '-q --quality-weight [number]',
+    'Specify a quality weight. Default: 1.95'
+  )
+  .option(
+    '-p --popularity-weight [number]',
+    'Specify a popularity weight. Default: 3.3'
+  )
+  .option(
+    '-m --maintenance-weight [number]',
+    'Specify a maintenance weight. Default: 2.05'
+  )
   .parse(process.argv)
 
-const plusify = (terms) => {
+const plusify = terms => {
   let result
   if (!terms[0]) {
     console.error('You need to pass search terms!')
     process.exit()
   }
-  if (typeof terms == 'array' ? result = terms : result = [terms.join('+')])
-  if (program.author) result.push(`author:${program.author}`)
+  if (
+    typeof terms === 'array' ? (result = terms) : (result = [terms.join('+')])
+  ) {
+    if (program.author) result.push(`author:${program.author}`)
+  }
   if (program.scope) result.push(`scope:${program.scope}`)
   if (program.keywords) result.push(`keywords:${program.keywords}`)
   if (program.notDeprecated || program.notUnstable || program.notInsecure) {
@@ -52,9 +70,15 @@ const plusify = (terms) => {
   }
   if (program.noBoostExact) result.push('boost-exact:false')
   if (program.scoreEffect) result.push(`score-effect:${program.scoreEffect}`)
-  if (program.qualityWeight) result.push(`quality-weight:${program.qualityWeight}`)
-  if (program.popularityWeight) result.push(`popularity-weight:${program.popularityWeight}`)
-  if (program.maintenanceWeight) result.push(`maintenance-weight:${program.maintenanceWeight}`)
+  if (program.qualityWeight) {
+    result.push(`quality-weight:${program.qualityWeight}`)
+  }
+  if (program.popularityWeight) {
+    result.push(`popularity-weight:${program.popularityWeight}`)
+  }
+  if (program.maintenanceWeight) {
+    result.push(`maintenance-weight:${program.maintenanceWeight}`)
+  }
   return result.join('+')
 }
 
@@ -63,7 +87,7 @@ const longest = a => {
     d = 0,
     l = 0,
     i = a.length
-  if (i)
+  if (i) {
     while (i--) {
       d = a[i].length
       if (d > c) {
@@ -71,20 +95,57 @@ const longest = a => {
         c = d
       }
     }
+  }
   return a[l].length
 }
 
-const stat = s => {
-  const f = Math.floor(s * 100).toString() + '%'
-  return f.length < 3 ? f.padStart(3) : f
+const stat = (s, type) => {
+  let f = Math.floor(s * 100).toString() + '%'
+  f = f.length < 3 ? f.padStart(3) : f
+  let output
+  switch (type) {
+    case 'quality':
+      output = chalk.green.dim(f)
+      break
+    case 'popularity':
+      output = chalk.yellow.dim(f)
+      break
+    case 'maintenance':
+      output = chalk.blue.dim(f)
+      break
+    default:
+      output = chalk.bgRed(f)
+  }
+  return output
 }
 
-const padFront = (s, n) => {
-  return s.length < n ? s.padStart(n) : s
+const name = (n, longN) => {
+  return chalk.bold(padBack(n, longN))
+}
+
+const version = (v, longV) => {
+  return chalk.gray.dim(padBack(v, longV))
 }
 
 const padBack = (s, n) => {
   return s.length < n ? s.padEnd(n) : s
+}
+
+const formatResult = (pkg, scores, longestName, longestVersion) => {
+  return {
+    name: `${name(pkg.name, longestName)} ${version(
+      pkg.version,
+      longestVersion
+    )} ${stat(scores.detail.quality, 'quality')} ${stat(
+      scores.detail.popularity,
+      'popularity'
+    )} ${stat(scores.detail.maintenance, 'maintenance')} ${stat(
+      scores.final
+    )} ${chalk.dim(pkg.description)}`,
+    value: {
+      url: pkg.links.npm
+    }
+  }
 }
 
 const search = terms => {
@@ -98,20 +159,9 @@ const search = terms => {
         results.results.map(n => n.package.version)
       )
 
-      const choices = results.results.map(n => ({
-        name: `${chalk.bold(
-          padBack(n.package.name, longestName)
-        )} ${chalk.gray.dim(
-          padBack(n.package.version, longestVersion)
-        )} ${chalk.green.dim(stat(n.score.detail.quality))} ${chalk.yellow.dim(
-          stat(n.score.detail.popularity)
-        )} ${chalk.blue.dim(stat(n.score.detail.maintenance))} ${chalk.bgRed(
-          stat(n.score.final)
-        )} ${chalk.dim(n.package.description)}`,
-        value: {
-          url: n.package.links.npm
-        }
-      }))
+      const choices = results.results.map(n =>
+        formatResult(n.package, n.score, longestName, longestVersion)
+      )
 
       inquirer
         .prompt({
@@ -121,7 +171,7 @@ const search = terms => {
 ${chalk.green.dim('<Quality>')} ${chalk.yellow.dim(
             '<Popularity>'
           )} ${chalk.blue.dim('<Maintenance>')} ${chalk.bgRed('<Overall>')}`,
-          choices: [ ...choices, new inquirer.Separator() ]
+          choices: [...choices, new inquirer.Separator()]
         })
         .then(answer => {
           open(answer.lib.url)
