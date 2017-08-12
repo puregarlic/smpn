c550b28074ec95f05586f8e4aaaf5992632a9ab5
Graham Barber <grahammbarber@gmail.com>
2017-08-11 21:31:59 -0700

    Added support for all npms filters

--- index.js
+++ index.js
@@ -12,25 +12,50 @@ program
   .usage('[options] [search terms]')
   .option('-a --author [name]', 'Search for a specific author')
   .option('-s --scope [scope]', 'Restrict search to a specific scope')
-  .option('-k --keywords [keywords]', 'Filter results to packages with the specific keywords')
+  .option('-k --keywords [keywords]', 'Filter results to packages with the specific keywords. Comma seperated, no spaces in between!')
   .option('-d --not-deprecated', 'Restrict to non-deprecated results')
   .option('-D --deprecated', 'Restrict to deprecated results')
-  .option('-u --stable', 'Restrict to stable results')
-  .option('-U --unstable', 'Restrict to unstable results')
-  .option('-i --secure', 'Restrict to secure results')
-  .option('-b --boost-exact', 'Disable NPMS\' boost exact functionality')
+  .option('-u --not-unstable', 'Restrict to results above v1.0.0')
+  .option('-U --unstable', 'Restrict to results beneath v1.0.0')
+  .option('-i --not-insecure', 'Restrict to secure results')
+  .option('-I --insecure', 'Restrict to insecure/vulnerable results')
+  .option('-b --no-boost-exact', 'Do not boost exact search matches')
   .option('-e --score-effect [number]', 'Set the score effect value. Default: 15.3')
-  .option('-q --quality [number]', 'Specify a quality weight. Default: 1.95')
-  .option('-p --popularity [number]', 'Specify a popularity weight. Default: 3.3')
-  .option('-m --maintenance [number]', 'Specify a maintenance weight. Default: 2.05')
+  .option('-q --quality-weight [number]', 'Specify a quality weight. Default: 1.95')
+  .option('-p --popularity-weight [number]', 'Specify a popularity weight. Default: 3.3')
+  .option('-m --maintenance-weight [number]', 'Specify a maintenance weight. Default: 2.05')
   .parse(process.argv)
 
-const validate = (strings, terms) => {
+const plusify = (terms) => {
+  let result
   if (!terms[0]) {
     console.error('You need to pass search terms!')
     process.exit()
   }
-  return strings[0] + (typeof terms == 'array' ? terms.join('+') : terms)
+  if (typeof terms == 'array' ? result = terms : result = [terms.join('+')])
+  if (program.author) result.push(`author:${program.author}`)
+  if (program.scope) result.push(`scope:${program.scope}`)
+  if (program.keywords) result.push(`keywords:${program.keywords}`)
+  if (program.notDeprecated || program.notUnstable || program.notInsecure) {
+    let not = []
+    if (program.notDeprecated) not.push('deprecated')
+    if (program.notUnstable) not.push('unstable')
+    if (program.notInsecure) not.push('insecure')
+    result.push(`not:${not.join(',')}`)
+  }
+  if (program.deprecated || program.unstable || program.insecure) {
+    let is = []
+    if (program.deprecated) is.push('deprecated')
+    if (program.unstable) is.push('unstable')
+    if (program.insecure) is.push('insecure')
+    result.push(`is:${is.join(',')}`)
+  }
+  if (program.noBoostExact) result.push('boost-exact:false')
+  if (program.scoreEffect) result.push(`score-effect:${program.scoreEffect}`)
+  if (program.qualityWeight) result.push(`quality-weight:${program.qualityWeight}`)
+  if (program.popularityWeight) result.push(`popularity-weight:${program.popularityWeight}`)
+  if (program.maintenanceWeight) result.push(`maintenance-weight:${program.maintenanceWeight}`)
+  return result.join('+')
 }
 
 const longest = a => {
@@ -63,7 +88,7 @@ const padBack = (s, n) => {
 }
 
 const search = terms => {
-  got(validate`https://api.npms.io/v2/search?q=${terms}`)
+  got(`https://api.npms.io/v2/search?q=` + plusify(terms))
     .then(res => {
       const results = JSON.parse(res.body)
       if (results.total === 0) return console.error('No results found!')
